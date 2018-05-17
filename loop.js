e6969d921f1b4bb3c38d37da147ff41193898949
gaspard macelin <gaspard@gaspardbruno.com>
2018-05-17 14:36:52 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,7 +1,11 @@
+console.log('ready');
 videojs.registerPlugin('loopVideo', function() {
   var myPlayer = this;
   
+  console.log('go');
+  
   myPlayer.on("ended", function () {
     myPlayer.play();
+     console.log('ended');
   });
 });
